e30ac7119340e6f8a7341ef55e5581fdb156e9ed
Anahi Salgado <anahi@platzi.com>
2017-06-27 14:09:24 -0500

    Implementando Listas en Xamarin Forms

---
+++ ToDoForms/ListaTareas.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ToDoForms
+{
+    public partial class ListaTareas : ContentPage
+    {
+        public ListaTareas()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ToDoForms/ToDoFormsPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+
+namespace ToDoForms
+{
+    public partial class ToDoFormsPage : ContentPage
+    {
+        public ToDoFormsPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            if(string.IsNullOrEmpty(usuarioEntry.Text) || string.IsNullOrEmpty(contraseñaEntry.Text))
+            {
+                resultadoLabel.Text = "Debe escribir usuario y contraseña";
+            }
+            else
+            {
+                resultadoLabel.Text = "Inicio de sesion exitoso";
+                await Navigation.PushAsync(new NuevoItem());
+            }
+        }
+    }
+}
